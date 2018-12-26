e24af8d81aea1bc68fc94622d2c70ff966bdc28a
raamenn <raamenn92@gmail.com>
2018-12-26 17:09:36 +0800

    An easy todo list with vue.js

---
+++ src/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="inputValue">
+    <button @click="handleClick">Update</button>
+    <ul>
+      <todo-item v-for="(item, index) of list" :key="index" :todoItem="item" :index="index" @delete="handleDelete"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoItem from './components/TodoItem'
+export default {
+  components: {
+    'todo-item': todoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleClick () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="handleDelete">{{todoItem}}</li>
+</template>
+
+<script>
+export default {
+  props: ['index', 'todoItem'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
